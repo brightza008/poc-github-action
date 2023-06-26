5ef2de625b6acc3c747c6a01e50b8b3a0a9e6fee
PONGSATHORN UPAN <pongsathorn.upan@scb.co.th>
2023-06-26 16:26:57 +0700

    bbb

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ let http = require('http');
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello');
+    res.end('Hello22');
 }).listen(1234, () => console.log('Server running on localhost:1234...')) ;
